054f31f36949ba181e9515a6505dec3522eaa2b0
Mikael <mjorda@stanford.edu>
2019-11-28 15:07:48 -0800

    prepared IK solver for floating hand

--- 12-uniGrasp_allegro/prev_solveIKAllegroAndArm.cpp
+++ 12-uniGrasp_allegro/prev_solveIKAllegroAndArm.cpp
@@ -16,10 +16,10 @@ using namespace Eigen;
 
 const string world_file = "./resources/world_IK.urdf";
 const vector<string> robot_files = {
-	"./resources/floatingAllegroHand.urdf",
+	"./resources/panda_arm_AllegroHand.urdf",
 };
 const vector<string> robot_names = {
-	"ALLEGRO_HAND",
+	"PANDA_HAND",
 };
 const string camera_name = "camera_fixed";
 
@@ -27,9 +27,9 @@ const int n_robots = robot_names.size();
 
 // redis keys:
 // - read:
-const string CAMERA_POS_IN_WORLD_KEY = "sai2::PandaApplication::camera::camera_pos_in_world";
-const string CAMERA_ROT_IN_WORLD_KEY = "sai2::PandaApplication::camera::camera_rot_in_world";
-const string DESIRED_FINGERTIP_POSITIONS_IN_CAMERA_FRAME_KEY = "sai2::PandaApplication::desired_fingertip_pos_in_camera_frame";
+const string ROBOT_JOINT_ANGLES_KEY = "sai2::FrankaPanda::Clyde::sensors::q";
+const string HAND_JOINT_ANGLES_KEY = "sai2::allegroHand::sensors::joint_positions";
+const string DESIRED_FINGERTIP_POSITIONS_KEY = "sai2::PandaApplication::desired_fingertip_pos_in_world_frame";
 
 // - write
 const string ROBOT_DESIRED_PALM_POSITION_KEY = "sai2::unigraspAllegro::desired_palm_position_from_IK";
@@ -75,29 +75,28 @@ int main() {
 	// load robots
 	Sai2Model::Sai2Model* robot = new Sai2Model::Sai2Model(robot_files[0], false);
 	int dof = robot->dof();
-	const string palm_link = "palm";
-	const Vector3d palm_pos_in_link = Vector3d(0,0,0);
+	const string palm_link = "link7";
+	const Vector3d palm_pos_in_link = Vector3d(0.05, 0.0, 0.15);
+	robot->_q.head(7) << 0.212565,-0.523804,-0.154537,-2.13458,-0.0625216,1.71954,-0.0469045;
 
-	robot->_q(2) = 0.5;
-	robot->_q(3) = M_PI/2;
 
 	// joint limits
 	VectorXd q_min = VectorXd::Zero(dof);
 	VectorXd q_max = VectorXd::Zero(dof);
-	VectorXd q_weights = VectorXd::Ones(dof);
+	VectorXd q_weights = VectorXd::Zero(dof);
 
-	q_min.head(6) = -1000.0 * VectorXd::Ones(6);
-	q_max.head(6) = 1000.0 * VectorXd::Ones(6);
-	// q_weights.head(6) = VectorXd::Ones(6);
+	q_min.head(7) << -2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -0.0175, -2.8973;
+	q_max.head(7) << 2.8973, 1.7628, 2.8973, -0.0698, 2.8973, 3.7525, 2.8973;
+	q_weights.head(7) = VectorXd::Ones(7);
+	q_weights(5) = 0.001;
+	q_weights(6) = 0.001;
 
 	q_min.tail(16) << -0.47, -0.196, -0.174, -0.227, -0.47, -0.196, -0.174, -0.227, -0.47, -0.196, -0.174, -0.227, -1.396, -0.105, -0.189, -0.162;
 	q_max.tail(16) << 0.47, 1.61, 1.709, 1.618, 0.47, 1.61, 1.709, 1.618, 0.47, 1.61, 1.709, 1.618, -0.263, 1.163, 1.644, 1.719;
-	// q_weights.tail(16) = 1000.0*VectorXd::Ones(16);
+	q_weights.tail(16) = 1000.0*VectorXd::Ones(16);
 
-	robot->_q.tail(16) = (2*q_min.tail(16) + q_max.tail(16))/3.0;
-	robot->_q(6) = 0;
-	robot->_q(10) = 0;
-	robot->_q(14) = 0;
+	// cout << q_weights.transpose() << endl;
+
 
 	// prepare IK points
 	vector<string> ik_links = {
@@ -118,55 +117,41 @@ int main() {
 		Vector3d(0.5,-0.05,0.5),
 	};
 
-	if(!redis_client.exists(CAMERA_POS_IN_WORLD_KEY))
+	if(!redis_client.exists(ROBOT_JOINT_ANGLES_KEY))
 	{
-		redis_client.setEigenMatrixJSON(CAMERA_POS_IN_WORLD_KEY, Vector3d::Zero());
+		redis_client.setEigenMatrixJSON(ROBOT_JOINT_ANGLES_KEY, robot->_q.head(7));
 	}
-	if(!redis_client.exists(CAMERA_ROT_IN_WORLD_KEY))
+	if(!redis_client.exists(HAND_JOINT_ANGLES_KEY))
 	{
-		redis_client.setEigenMatrixJSON(CAMERA_ROT_IN_WORLD_KEY, Matrix3d::Identity());
+		redis_client.setEigenMatrixJSON(HAND_JOINT_ANGLES_KEY, VectorXd::Zero(16));
 	}
-	if(!redis_client.exists(DESIRED_FINGERTIP_POSITIONS_IN_CAMERA_FRAME_KEY))
+	if(!redis_client.exists(DESIRED_FINGERTIP_POSITIONS_KEY))
 	{
 		VectorXd tmp_des_ft_pos = VectorXd::Zero(9);
 		tmp_des_ft_pos.segment<3>(0) = ik_desired_positions[0];
 		tmp_des_ft_pos.segment<3>(3) = ik_desired_positions[1];
 		tmp_des_ft_pos.segment<3>(6) = ik_desired_positions[2];
-		redis_client.setEigenMatrixJSON(DESIRED_FINGERTIP_POSITIONS_IN_CAMERA_FRAME_KEY, tmp_des_ft_pos);
+		redis_client.setEigenMatrixJSON(DESIRED_FINGERTIP_POSITIONS_KEY, tmp_des_ft_pos);
 	}
 
-	// read transform between world and camera from redis, and the desired contact points in camera frame
-	Affine3d T_world_camera = Affine3d::Identity();
-	T_world_camera.translation() = redis_client.getEigenMatrixJSON(CAMERA_POS_IN_WORLD_KEY);
-	T_world_camera.linear() = redis_client.getEigenMatrixJSON(CAMERA_ROT_IN_WORLD_KEY);
-	VectorXd tmp_fingertip_desired_pos = redis_client.getEigenMatrixJSON(DESIRED_FINGERTIP_POSITIONS_IN_CAMERA_FRAME_KEY);
-
-	// transform the points to world frame
-	ik_desired_positions[0] = T_world_camera * tmp_fingertip_desired_pos.segment<3>(0);
-	ik_desired_positions[1] = T_world_camera * tmp_fingertip_desired_pos.segment<3>(3);
-	ik_desired_positions[2] = T_world_camera * tmp_fingertip_desired_pos.segment<3>(6);
+	// read from redis the current robot configuration and contact points
+	robot->_q.head(7) = redis_client.getEigenMatrixJSON(ROBOT_JOINT_ANGLES_KEY);
+	robot->_q.tail(16) = redis_client.getEigenMatrixJSON(HAND_JOINT_ANGLES_KEY);
+	VectorXd tmp_fingertip_desired_pos = redis_client.getEigenMatrixJSON(DESIRED_FINGERTIP_POSITIONS_KEY);
+	ik_desired_positions[0] = tmp_fingertip_desired_pos.segment<3>(0);
+	ik_desired_positions[1] = tmp_fingertip_desired_pos.segment<3>(3);
+	ik_desired_positions[2] = tmp_fingertip_desired_pos.segment<3>(6);
 	
-	// find a good initialization for the position of the hand 
-	// make z hand coincide with the axis from the thumb contact point to the index contact poinr
-	// put the hand above the thumb contact point by 10cm
-	Vector3d p_thumb_index = ik_desired_positions[1] - ik_desired_positions[0];
-	Vector3d p_index_middle = ik_desired_positions[2] - ik_desired_positions[1];
-	p_thumb_index.normalize();
-	p_index_middle.normalize();
-	double angle = atan2(p_thumb_index(1), p_thumb_index(0));
-	robot->_q(4) = -angle;
-
-	robot->_q(0) = ik_desired_positions[0](0);
-	robot->_q(1) = ik_desired_positions[0](1);
-	robot->_q(2) = ik_desired_positions[0](2) + 0.12;
-	robot->_q.head(3) += 0.05 * p_index_middle;
-
 	robot->updateKinematics();
+
+	robot->position(desired_palm_position, palm_link, palm_pos_in_link);
+	robot->rotation(desired_palm_orientation, palm_link);
+	desired_finger_configuration = robot->_q.tail(16);
 
 	// compute inverse kinematics
 	VectorXd q_ik = VectorXd::Zero(dof);	
+
 	robot->computeIK3d_JL(q_ik, ik_links, ik_pos_in_link, ik_desired_positions, q_min, q_max, q_weights);
-	// robot->computeIK3d(q_ik, ik_links, ik_pos_in_link, ik_desired_positions);
 	robot->_q = q_ik;
 	robot->updateKinematics();
 
@@ -176,17 +161,9 @@ int main() {
 	desired_finger_configuration = robot->_q.tail(16);
 	desired_finger_configuration.segment<4>(8) << 0.0, 0.1, 0.1, 0.05;
 
-	/*------- Set up visualization -------*/
-	// display contact points
-	vector<chai3d::cShapeSphere*> graphic_contact_points;
-	for(int i=0 ; i<3 ; i++)
-	{
-		graphic_contact_points.push_back(new chai3d::cShapeSphere(0.015));
-		graphic_contact_points[i]->m_material->setColorf(1.0,0.0,0.0);
-		graphics->_world->addChild(graphic_contact_points[i]);
-		graphic_contact_points[i]->setLocalPos(ik_desired_positions[i]);
-	}
+	cout << desired_palm_orientation * desired_palm_orientation.transpose() << endl;
 
+	/*------- Set up visualization -------*/
 	// set up error callback
 	glfwSetErrorCallback(glfwError);
 
